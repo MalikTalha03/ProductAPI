75f69d697d8ab3667eb80d75d6508e0f120909da
MalikTalha03 <mtnaqshbandi2003@gmail.com>
2023-11-07 22:24:12 +0500

    Changed JS

--- index.js
+++ index.js
@@ -1,95 +1,101 @@
-$(function () {
-    loadProduct();
-    $("#recipes").on("click", ".btn-danger", delProduct);
-    $("#recipes").on("click", ".btn-warning", updateProduct);
-    $("#addBtn").on("click", addProduct);
-    $("#updateSave").on("click",function () {
-        var id = $("#updateId").val();
-        var name = $("#updateTitle").val();
-        var price = $("#updatePrice").val();
-        var color = $("#updateColor").val();
-        var department = $("#updateDept").val();
-        var description = $("#updateDesc").val();
-      $.ajax({
-        url: "https://usman-fake-api.herokuapp.com/api/products/" + id,
-        data: { name, price, color,department, description },
-        method: "PUT",
-        success: function (response) {
-            loadProduct();
+$(document).ready(function () {
+  loadProduct();
+  $("#recipes").on("click", ".btn-danger", function() {
+      delProduct($(this).closest(".recipe").data("id"));
+  });
+  $("#recipes").on("click", ".btn-warning", function() {
+      updateProduct($(this).closest(".recipe").data("id"));
+  });
+  $("#addBtn").on("click", addProduct);
+  $("#updateSave").on("click", updateProductData);
+});
+
+function updateProductData() {
+  const id = $("#updateId").val();
+  const name = $("#updateTitle").val();
+  const price = $("#updatePrice").val();
+  const color = $("#updateColor").val();
+  const department = $("#updateDept").val();
+  const description = $("#updateDesc").val();
+
+  $.ajax({
+      url: `https://usman-fake-api.herokuapp.com/api/products/${id}`,
+      data: { name, price, color, department, description },
+      method: "PUT",
+      success: function (response) {
+          loadProduct();
           $("#updateModal").modal("hide");
-        },
-      });
-    });
-    function updateProduct() {
-    var btn = $(this);
-    var parentDiv = btn.closest(".recipe");
-    let id = parentDiv.attr("data-id");
-    $.get(
-      "https://usman-fake-api.herokuapp.com/api/products/" + id,
-      function (response) {
-        $("#updateId").val(response._id);
-        $("#updateTitle").val(response.name);
-        $("#updatePrice").val(response.price);
-        $("#updateColor").val(response.color);
-        $("#updateDept").val(response.department);
-        $("#updateDesc").val(response.description);
-        $("#updateModal").modal("show");
-      }
-    );
-  }
-  function addProduct() {
-    var name = $("#title").val();
-    var color = $("#color").val();
-    var price = $("#price").val();
-    var department = $("#dept").val();
-    var description = $("#desc").val();
-    $.ajax({
+      },
+  });
+}
+
+function updateProduct(id) {
+  $.get(`https://usman-fake-api.herokuapp.com/api/products/${id}`, function (response) {
+      $("#updateId").val(response._id);
+      $("#updateTitle").val(response.name);
+      $("#updatePrice").val(response.price);
+      $("#updateColor").val(response.color);
+      $("#updateDept").val(response.department);
+      $("#updateDesc").val(response.description);
+      $("#updateModal").modal("show");
+  });
+}
+
+function addProduct() {
+  const name = $("#title").val();
+  const color = $("#color").val();
+  const price = $("#price").val();
+  const department = $("#dept").val();
+  const description = $("#desc").val();
+
+  $.ajax({
       url: "https://usman-fake-api.herokuapp.com/api/products",
       method: "POST",
-      data: { name, price, color,department, description },
+      data: { name, price, color, department, description },
       success: function (response) {
-        $("#title").val("");
-        $("#color").val("");
-        $("#price").val("");
-        $("#dept").val("");
-        $("#desc").val("");
-        loadProduct();
-        $("#addModal").modal("hide");
+          $("#title, #color, #price, #dept, #desc").val("");
+          loadProduct();
+          $("#addModal").modal("hide");
       },
-    });
-  }
-  function delProduct() {
-    var btn = $(this);
-    var parentDiv = btn.closest(".recipe");
-    let id = parentDiv.attr("data-id");
-    $.ajax({
-      url: "https://usman-fake-api.herokuapp.com/api/products/" + id,
+  });
+}
+
+function delProduct(id) {
+  $.ajax({
+      url: `https://usman-fake-api.herokuapp.com/api/products/${id}`,
       method: "DELETE",
       success: function () {
-        loadProduct();
+          loadProduct();
       },
-    });
-  }
-  function loadProduct() {
-    $.ajax({
+  });
+}
+
+function loadProduct() {
+  $.ajax({
       url: "https://usman-fake-api.herokuapp.com/api/products",
       method: "GET",
       error: function (response) {
-        var recipes = $("#recipes");
-        recipes.html("An Error has occured");
+          const recipes = $("#recipes");
+          recipes.html("An Error has occurred");
       },
       success: function (response) {
-        var recipes = $("#recipes");
-        recipes.empty();
-        for (var i = 0; i < response.length; i++) {
-          var rec = response[i];
-          recipes.append(
-            `<div class="recipe" data-id="${rec._id}"><h3>${rec.name} <p>Color : ${rec.color}</p></h3><p>Rs. ${rec.price}</p><p>${rec.department}</p><p><button class="btn btn-danger btn-sm float-right">delete</button><button class="btn btn-warning btn-sm float-right">Edit</button><p>${rec.description}</p></div>`
-          );
-        }
+          const recipes = $("#recipes");
+          recipes.empty();
+          for (let i = 0; i < response.length; i++) {
+              const rec = response[i];
+              recipes.append(
+                  `<div class="recipe" data-id="${rec._id}">
+                      <h3>${rec.name}<p>Color : ${rec.color}</p></h3>
+                      <p>Rs. ${rec.price}</p>
+                      <p>${rec.department}</p>
+                      <p>
+                          <button class="btn btn-danger btn-sm float-right">delete</button>
+                          <button class="btn btn-warning btn-sm float-right">Edit</button>
+                      </p>
+                      <p>${rec.description}</p>
+                  </div>`
+              );
+          }
       },
-    });
-  }
-  
   });
-  
+}
